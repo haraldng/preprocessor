49036905631a6ec2d2159ab03ed37b8b610d92e2
Kun Wu <kun.wu@eyevinn.se>
2022-12-19 11:03:25 +0100

    add lecar

--- src/cache.rs
+++ src/cache.rs
@@ -3,27 +3,35 @@ use lfu_cache::LfuCache;
 use lru::LruCache;
 use std::marker::PhantomData;
 use std::num::NonZeroUsize;
+use lecar::controller::Controller;
+use strum_macros::EnumIter; 
 
 pub type CacheKey = String;
 
+#[derive(Debug, Eq, PartialEq, EnumIter)]
+pub enum CacheType {
+    LFU,
+    LRU,
+    LECAR
+}
+
 /// The cache we use in paxos
 pub struct CacheModel {
-    use_lfu: bool,
+    cache_type: CacheType,
     lfu_cache: LfuCache<CacheKey, PhantomData<u32>>,
     lru_cache: LruCache<CacheKey, PhantomData<u32>>,
+    lecar_cache: Controller,
     index_cache: IndexMap<CacheKey, PhantomData<u32>>,
 }
 
 impl CacheModel {
     /// create a new cache model
-    pub fn with(capacity: usize, use_lfu: bool) -> Self {
-        let lfu_cache_capacity = if use_lfu { capacity } else { 1 };
-        let lru_cache_capacity = if use_lfu { 1 } else { capacity };
-
+    pub fn with(capacity: usize, cache_type: CacheType) -> Self {
         CacheModel {
-            use_lfu,
-            lfu_cache: LfuCache::with_capacity(lfu_cache_capacity),
-            lru_cache: LruCache::new(NonZeroUsize::new(lru_cache_capacity).unwrap()),
+            cache_type,
+            lfu_cache: LfuCache::with_capacity(capacity),
+            lru_cache: LruCache::new(NonZeroUsize::new(capacity).unwrap()),
+            lecar_cache: Controller::new(capacity, capacity, capacity),
             index_cache: IndexMap::with_capacity(capacity),
         }
     }
@@ -31,10 +39,10 @@ impl CacheModel {
     /// save (key, value) pair into cache
     /// Optimization: The value could be skipped if it always equals to the key
     pub fn put(&mut self, key: CacheKey) {
-        if self.use_lfu {
-            self.lfu_cache.insert(key.clone(), PhantomData);
-        } else {
-            self.lru_cache.put(key.clone(), PhantomData);
+        match self.cache_type {
+            CacheType::LFU => { self.lfu_cache.insert(key.clone(), PhantomData); },
+            CacheType::LRU => { self.lru_cache.put(key.clone(), PhantomData); },
+            CacheType::LECAR => { self.lecar_cache.insert(&key, 0); },
         }
 
         self.index_cache.insert(key, PhantomData);
@@ -42,13 +50,10 @@ impl CacheModel {
 
     /// update the frequency or recency of the template.
     pub fn update_cache(&mut self, key: &CacheKey) {
-        if self.use_lfu {
-            let _ = self
-                .lfu_cache
-                .get(key)
-                .expect("Tried to update frequency of cache item that doesn't exist.");
-        } else {
-            self.lru_cache.promote(key);
+        match self.cache_type {
+            CacheType::LFU => { self.lfu_cache.get(key).expect("Tried to update frequency of cache item that doesn't exist."); },
+            CacheType::LRU => { self.lru_cache.promote(key); },
+            CacheType::LECAR => { self.lecar_cache.get(key).expect("Tried to update frequency of cache item that doesn't exist.");  },
         }
     }
 
@@ -64,10 +69,10 @@ impl CacheModel {
 
     /// return cache length
     pub fn len(&self) -> usize {
-        if self.use_lfu {
-            self.lfu_cache.len()
-        } else {
-            self.lru_cache.len()
+        match self.cache_type {
+            CacheType::LFU => self.lfu_cache.len(),
+            CacheType::LRU => self.lru_cache.len(),
+            CacheType::LECAR => self.lecar_cache.len().0,
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,77 +5,83 @@ mod preprocess;
 use crate::preprocess::{decode, encode};
 use histogram::Histogram;
 use std::time::Instant;
+use strum::IntoEnumIterator;
 
 fn main() {
     let commands = load::read_from_file("queries.txt");
-    let mut cache = cache::CacheModel::with(500, true);
     let num_commands = commands.len();
     println!("Total number of commands: {}", num_commands);
+    for cache_type in cache::CacheType::iter() {
+        println!("--------------------------------");
+        println!("Measuring cache type: {:?}", cache_type);
 
-    let mut encode_histo = Histogram::new();
-    let mut decode_histo = Histogram::new();
-    let mut query_len_histo = Histogram::new();
-    let mut compression_histo = Histogram::new();
-    let mut hit_count = 0;
+        let mut cache = cache::CacheModel::with(500, cache_type);
+        let mut encode_histo = Histogram::new();
+        let mut decode_histo = Histogram::new();
+        let mut query_len_histo = Histogram::new();
+        let mut compression_histo = Histogram::new();
+        let mut hit_count = 0;
 
-    // run with checks
-    for command in &commands[0..] {
-        let mut raw_command = command.clone();
-        let start = Instant::now();
-        let (hit, compression_rate) = encode(&mut raw_command, &mut cache);
-        let encode_end = Instant::now();
-        decode(&mut raw_command, &mut cache);
-        let decode_end = Instant::now();
+        // run with checks
+        for command in &commands[0..] {
+            let mut raw_command = command.clone();
+            let start = Instant::now();
+            let (hit, compression_rate) = encode(&mut raw_command, &mut cache);
+            let encode_end = Instant::now();
+            decode(&mut raw_command, &mut cache);
+            let decode_end = Instant::now();
 
-        assert_eq!(raw_command.sql, command.sql);
+            assert_eq!(raw_command.sql, command.sql);
 
-        let encode_time = encode_end.duration_since(start).as_nanos();
-        let decode_time = decode_end.duration_since(encode_end).as_nanos();
-        encode_histo.increment(encode_time as u64).unwrap();
-        decode_histo.increment(decode_time as u64).unwrap();
-        query_len_histo.increment(command.sql.len() as u64).unwrap();
-        compression_histo.increment(compression_rate as u64).unwrap();
-        if hit { hit_count += 1; }
+            let encode_time = encode_end.duration_since(start).as_nanos();
+            let decode_time = decode_end.duration_since(encode_end).as_nanos();
+            encode_histo.increment(encode_time as u64).unwrap();
+            decode_histo.increment(decode_time as u64).unwrap();
+            query_len_histo.increment(command.sql.len() as u64).unwrap();
+            compression_histo.increment(compression_rate as u64).unwrap();
+            if hit { hit_count += 1; }
+        }
+        let hit_rate = hit_count as f32 / num_commands as f32;
+
+        println!("Number of commands: {}", num_commands);
+        println!(
+            "Encoding (ns): Avg: {}, p50: {}, p95: {}, Min: {}, Max: {}, StdDev: {}",
+            encode_histo.mean().unwrap(),
+            encode_histo.percentile(50f64).unwrap(),
+            encode_histo.percentile(95f64).unwrap(),
+            encode_histo.minimum().unwrap(),
+            encode_histo.maximum().unwrap(),
+            encode_histo.stddev().unwrap(),
+        );
+        println!(
+            "Decoding (ns): Avg: {}, p50: {}, p95: {}, Min: {}, Max: {}, StdDev: {}",
+            decode_histo.mean().unwrap(),
+            decode_histo.percentile(50f64).unwrap(),
+            decode_histo.percentile(95f64).unwrap(),
+            decode_histo.minimum().unwrap(),
+            decode_histo.maximum().unwrap(),
+            decode_histo.stddev().unwrap(),
+        );
+        println!(
+            "Query length: Avg: {}, p50: {}, p95: {}, Min: {}, Max: {}, StdDev: {}",
+            query_len_histo.mean().unwrap(),
+            query_len_histo.percentile(50f64).unwrap(),
+            query_len_histo.percentile(95f64).unwrap(),
+            query_len_histo.minimum().unwrap(),
+            query_len_histo.maximum().unwrap(),
+            query_len_histo.stddev().unwrap(),
+        );
+        println!(
+            "Compression Rate (%): Avg: {}, p50: {}, p95: {}, Min: {}, Max: {}, StdDev: {}",
+            compression_histo.mean().unwrap(),
+            compression_histo.percentile(50f64).unwrap(),
+            compression_histo.percentile(95f64).unwrap(),
+            compression_histo.minimum().unwrap(),
+            compression_histo.maximum().unwrap(),
+            compression_histo.stddev().unwrap(),
+        );
+
+        println!("Hit rate: {}", hit_rate);
+        
     }
-    let hit_rate = hit_count as f32 / num_commands as f32;
-
-    println!("Number of commands: {}", num_commands);
-    println!(
-        "Encoding (ns): Avg: {}, p50: {}, p95: {}, Min: {}, Max: {}, StdDev: {}",
-        encode_histo.mean().unwrap(),
-        encode_histo.percentile(50f64).unwrap(),
-        encode_histo.percentile(95f64).unwrap(),
-        encode_histo.minimum().unwrap(),
-        encode_histo.maximum().unwrap(),
-        encode_histo.stddev().unwrap(),
-    );
-    println!(
-        "Decoding (ns): Avg: {}, p50: {}, p95: {}, Min: {}, Max: {}, StdDev: {}",
-        decode_histo.mean().unwrap(),
-        decode_histo.percentile(50f64).unwrap(),
-        decode_histo.percentile(95f64).unwrap(),
-        decode_histo.minimum().unwrap(),
-        decode_histo.maximum().unwrap(),
-        decode_histo.stddev().unwrap(),
-    );
-    println!(
-        "Query length: Avg: {}, p50: {}, p95: {}, Min: {}, Max: {}, StdDev: {}",
-        query_len_histo.mean().unwrap(),
-        query_len_histo.percentile(50f64).unwrap(),
-        query_len_histo.percentile(95f64).unwrap(),
-        query_len_histo.minimum().unwrap(),
-        query_len_histo.maximum().unwrap(),
-        query_len_histo.stddev().unwrap(),
-    );
-    println!(
-        "Compression Rate (%): Avg: {}, p50: {}, p95: {}, Min: {}, Max: {}, StdDev: {}",
-        compression_histo.mean().unwrap(),
-        compression_histo.percentile(50f64).unwrap(),
-        compression_histo.percentile(95f64).unwrap(),
-        compression_histo.minimum().unwrap(),
-        compression_histo.maximum().unwrap(),
-        compression_histo.stddev().unwrap(),
-    );
-
-    println!("Hit rate: {}", hit_rate);
 }
