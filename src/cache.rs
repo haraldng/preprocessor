aae4b2d2e401afea8d7413fe4b3427b012791c50
Kun Wu <kun.wu@eyevinn.se>
2023-01-02 04:28:46 +0100

    remove LeCaR

--- src/cache.rs
+++ src/cache.rs
@@ -3,7 +3,6 @@ use lfu_cache::LfuCache;
 use lru::LruCache;
 use std::marker::PhantomData;
 use std::num::NonZeroUsize;
-use lecar::controller::Controller;
 use strum_macros::EnumIter; 
 
 pub type CacheKey = String;
@@ -11,8 +10,7 @@ pub type CacheKey = String;
 #[derive(Debug, Eq, PartialEq, EnumIter)]
 pub enum CacheType {
     LFU,
-    LRU,
-    LECAR
+    LRU
 }
 
 /// The cache we use in paxos
@@ -20,7 +18,6 @@ pub struct CacheModel {
     cache_type: CacheType,
     lfu_cache: LfuCache<CacheKey, PhantomData<u32>>,
     lru_cache: LruCache<CacheKey, PhantomData<u32>>,
-    lecar_cache: Controller,
     index_cache: IndexMap<CacheKey, PhantomData<u32>>,
 }
 
@@ -31,7 +28,6 @@ impl CacheModel {
             cache_type,
             lfu_cache: LfuCache::with_capacity(capacity),
             lru_cache: LruCache::new(NonZeroUsize::new(capacity).unwrap()),
-            lecar_cache: Controller::new(capacity, capacity, capacity),
             index_cache: IndexMap::with_capacity(capacity),
         }
     }
@@ -42,7 +38,6 @@ impl CacheModel {
         match self.cache_type {
             CacheType::LFU => { self.lfu_cache.insert(key.clone(), PhantomData); },
             CacheType::LRU => { self.lru_cache.put(key.clone(), PhantomData); },
-            CacheType::LECAR => { self.lecar_cache.insert(&key, 0); },
         }
 
         self.index_cache.insert(key, PhantomData);
@@ -53,7 +48,6 @@ impl CacheModel {
         match self.cache_type {
             CacheType::LFU => { self.lfu_cache.get(key).expect("Tried to update frequency of cache item that doesn't exist."); },
             CacheType::LRU => { self.lru_cache.promote(key); },
-            CacheType::LECAR => { self.lecar_cache.get(key).expect("Tried to update frequency of cache item that doesn't exist.");  },
         }
     }
 
@@ -72,7 +66,6 @@ impl CacheModel {
         match self.cache_type {
             CacheType::LFU => self.lfu_cache.len(),
             CacheType::LRU => self.lru_cache.len(),
-            CacheType::LECAR => self.lecar_cache.len().0,
         }
     }
 }
